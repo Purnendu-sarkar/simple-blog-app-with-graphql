121fdad4754edff2de69b532ad1f47b4eb36e111
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2026-08-08 13:42:29 +0600

    Retrieving User Data

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,13 +1,21 @@
+import { prisma } from "../lib/prisma";
 
+interface userInfo {
+    name: string,
+    email: string,
+    password: string,
+}
 
 
 export const resolvers = {
     Query: {
-
+        users: async (parent: any, args: any, context: any) => {
+            return await prisma.user.findMany();
+        }
     },
     Mutation: {
-        signup: (parent: any, args: any, context: any) => {
-            console.log(args)
+        signup: async (parent: any, args: userInfo, context: any) => {
+            //console.log(args)
             return context.prisma.user.create({
                 data: args
             });
