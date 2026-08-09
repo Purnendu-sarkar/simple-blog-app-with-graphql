de55b767c7442900854f1058032b2ad25e9e3055
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2026-08-09 09:02:33 +0600

    Structuring Files and Defining Context Parameters

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,14 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './schema';
 import { resolvers } from './resolvers';
 import { prisma } from './lib/prisma';
+import { PrismaClient } from './generated/prisma/client';
+import { DefaultArgs } from '@prisma/client/runtime/client';
+import { GlobalOmitConfig } from './generated/prisma/internal/prismaNamespace';
+
+
+interface Context {
+    prisma: PrismaClient<never, GlobalOmitConfig | undefined, DefaultArgs>
+}
 
 const main = async () => {
     const server = new ApolloServer({
@@ -12,9 +20,11 @@ const main = async () => {
 
     const { url } = await startStandaloneServer(server, {
         listen: { port: 4000 },
-        context: async () => ({
-            prisma,
-        }),
+        context: async (): Promise<Context> => {
+            return {
+                prisma
+            }
+        },
     });
 
     console.log(`🚀  Server ready at: ${url}`);
---
+++ src/resolvers/Mutation/Mutation.ts
@@ -0,0 +1,90 @@
+import bcrypt from 'bcrypt';
+import { jwtHelper } from '../../utils/jwtHelper';
+import config from '../../config';
+
+interface userInfo {
+    name: string,
+    email: string,
+    password: string,
+    bio?: string
+}
+
+
+export const Mutation = {
+    signup: async (parent: any, args: userInfo, { prisma }: any) => {
+        const isExist = await prisma.user.findFirst({
+            where: {
+                email: args.email
+            }
+        })
+
+        if (isExist) {
+            return {
+                userError: "Already this email is Registered!",
+                token: null
+            }
+        }
+
+        const hashedPassword = await bcrypt.hash(args.password, 12)
+        //console.log(hashedPassword)
+        //console.log(args)
+        const newUser = await prisma.user.create({
+            data: {
+                name: args.name,
+                email: args.email,
+                password: hashedPassword
+            }
+        });
+
+        if (args.bio) {
+            await prisma.profile.create({
+                data: {
+                    bio: args.bio,
+                    userId: newUser.id
+                }
+            })
+        }
+
+        const token = await jwtHelper({ userID: newUser.id }, config.jwt.secret as string);
+        //console.log(token)
+        return {
+            userError: null,
+            token: token,
+        }
+    },
+
+    signin: async (parent: any, args: any, { prisma }: any) => {
+        console.log(args)
+
+        const user = await prisma.user.findFirst({
+            where: {
+                email: args.email
+            }
+        })
+
+        //console.log(user)
+
+        if (!user) {
+            return {
+                userError: "User Not Found!",
+                token: null
+            }
+        }
+
+        const correctPass = await bcrypt.compare(args.password, user.password);
+        //console.log(correctPass)
+
+        if (!correctPass) {
+            return {
+                userError: "Incorrect Password!",
+                token: null
+            }
+        }
+
+        const token = await jwtHelper({ userID: user.id }, config.jwt.secret as string);
+        return {
+            userError: null,
+            token: token,
+        }
+    }
+}
